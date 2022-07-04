8d99789e06640b747fe483047d8b77e1945914c5
saintabomination <saint.abomination@protonmail.com>
2022-07-05 00:12:27 +0200

    Getting user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-  println!("Hello, World!");
+	loop {
+		let mut input: String = String::new();
+		print!("> ");
+		std::io::stdout().flush().unwrap();
+
+		match io::stdin().read_line(&mut input) {
+			Ok(_) => println!("Good"),
+			Err(_) => println!("Bad"),
+		}
+	}
 }
